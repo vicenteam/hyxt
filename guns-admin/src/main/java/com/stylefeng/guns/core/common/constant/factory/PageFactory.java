daf701c811c42e230a6426d188a7f123356458aa
cyq <1328590438@qq.com>
2018-08-21 15:37:26 +0800

    会员统计分布图

--- guns-admin/src/main/java/com/stylefeng/guns/core/common/constant/factory/PageFactory.java
+++ guns-admin/src/main/java/com/stylefeng/guns/core/common/constant/factory/PageFactory.java
@@ -17,10 +17,18 @@ public class PageFactory<T> {
 
     public Page<T> defaultPage() {
         HttpServletRequest request = HttpKit.getRequest();
-        int limit = Integer.valueOf(request.getParameter("limit"));     //每页多少条数据
-        int offset = Integer.valueOf(request.getParameter("offset"));   //每页的偏移量(本页当前有多少条)
-        String sort = request.getParameter("sort");         //排序字段名称
-        String order = request.getParameter("order");       //asc或desc(升序或降序)
+        int limit =10;
+        int offset =1;
+        String sort="";
+        String order="";
+         try{
+             limit = Integer.valueOf(request.getParameter("limit"));     //每页多少条数据
+             offset = Integer.valueOf(request.getParameter("offset"));   //每页的偏移量(本页当前有多少条)
+             sort = request.getParameter("sort");         //排序字段名称
+             order = request.getParameter("order");       //asc或desc(升序或降序)
+         }catch (Exception e){
+
+         }
         if (ToolUtil.isEmpty(sort)) {
             Page<T> page = new Page<>((offset / limit + 1), limit);
             page.setOpenSort(false);
--- guns-admin/src/main/java/com/stylefeng/guns/modular/main/controller/BarRankingController.java
+++ guns-admin/src/main/java/com/stylefeng/guns/modular/main/controller/BarRankingController.java
@@ -1,15 +1,21 @@
 package com.stylefeng.guns.modular.main.controller;
 
+import com.baomidou.mybatisplus.plugins.Page;
 import com.stylefeng.guns.core.base.controller.BaseController;
 import com.stylefeng.guns.core.common.BaseEntityWrapper.BaseEntityWrapper;
+import com.stylefeng.guns.core.common.constant.factory.PageFactory;
 import com.stylefeng.guns.modular.main.service.IMembermanagementService;
+import com.stylefeng.guns.modular.main.service.IMembershipcardtypeService;
 import com.stylefeng.guns.modular.system.model.Membermanagement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 积分排名图表controller
@@ -22,6 +28,8 @@ public class BarRankingController extends BaseController {
 
     @Autowired
     private IMembermanagementService membermanagementService;
+    @Autowired
+    private IMembershipcardtypeService membershipcardtypeService;
 
     @RequestMapping("")
     public String index() {
@@ -31,11 +39,19 @@ public class BarRankingController extends BaseController {
     @RequestMapping("/list")
     @ResponseBody
     public Object list(){
+        Page<Membermanagement> page = new PageFactory<Membermanagement>().defaultPage();
         BaseEntityWrapper<Membermanagement> mWrapper = new BaseEntityWrapper<>();
         mWrapper.orderBy("integral",false);
-        List<Membermanagement> mList = membermanagementService.selectList(mWrapper);
-        System.out.println(" ---- * -----"+mList);
+        List<Membermanagement> mList =new ArrayList<>();// membermanagementService.selectList(mWrapper);
+        Page<Map<String,Object>> pagemap=membermanagementService.selectMapsPage(page,mWrapper);
+       List< Map<String,Object>> mapList=pagemap.getRecords();
+        mapList.forEach(a->{
+            String levelID=(String)a.get("levelID");
+            if(!StringUtils.isEmpty(levelID)){
+                a.put("levelName",membershipcardtypeService.selectById(levelID).getCardname());
+            }
+        });
 
-        return mList;
+        return mapList;
     }
 }
