d8f10222cd2bcca3bec597b8069a1444698d5e3d
chenyq <1418806370@qq.com>
2018-08-30 09:42:07 +0800

    daobao

--- guns-admin/src/main/java/com/stylefeng/guns/modular/main/controller/MembershipcardtypeController.java
+++ guns-admin/src/main/java/com/stylefeng/guns/modular/main/controller/MembershipcardtypeController.java
@@ -1,30 +1,23 @@
 package com.stylefeng.guns.modular.main.controller;
 
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.plugins.Page;
 import com.stylefeng.guns.core.base.controller.BaseController;
+import com.stylefeng.guns.core.common.constant.factory.PageFactory;
+import com.stylefeng.guns.core.log.LogObjectHolder;
 import com.stylefeng.guns.core.shiro.ShiroKit;
 import com.stylefeng.guns.core.util.DateUtil;
+import com.stylefeng.guns.modular.main.service.IMembershipcardtypeService;
 import com.stylefeng.guns.modular.system.model.Dept;
+import com.stylefeng.guns.modular.system.model.Membershipcardtype;
 import com.stylefeng.guns.modular.system.service.IDeptService;
-import com.sun.org.apache.xpath.internal.operations.Mod;
-import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import com.baomidou.mybatisplus.plugins.Page;
-import com.stylefeng.guns.core.common.constant.factory.PageFactory;
-import com.stylefeng.guns.core.common.BaseEntityWrapper.BaseEntityWrapper;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.beans.factory.annotation.Autowired;
-import com.stylefeng.guns.core.log.LogObjectHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import com.stylefeng.guns.modular.system.model.Membershipcardtype;
-import com.stylefeng.guns.modular.main.service.IMembershipcardtypeService;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 会员配置控制器
